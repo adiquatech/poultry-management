914fff864b48b3329aaaa204a20295adad230254
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-10-14 17:07:38 +0100

    vite corrected

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,12 @@ export default defineConfig({
         growth: resolve(__dirname, 'src/growth/index.html'),
         signin: resolve(__dirname, 'src/auth/signin.html'),
         signup: resolve(__dirname, 'src/auth/signup.html'),
-        callback: resolve(__dirname, 'src/auth/callback.html'),
       },
     },
   },
   server: {
     fs: {
-      allow: ['src'] // Allow serving files from src/ during development
+      allow: ['src']
     }
   }
 });
