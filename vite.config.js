8e74e6a325294adac0b5a57e021fd37fdec5bfe2
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-10-15 18:18:38 +0100

    netliy new try

--- vite.config.js
+++ vite.config.js
@@ -8,28 +8,16 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        growth: resolve(__dirname, 'src/growth/index.html'),
-        signin: resolve(__dirname, 'src/auth/signin.html'),
-        signup: resolve(__dirname, 'src/auth/signup.html'),
+        main: resolve(__dirname, 'index.html'),
+        growth: resolve(__dirname, 'growth/index.html'),
+        signin: resolve(__dirname, 'auth/signin.html'),
+        signup: resolve(__dirname, 'auth/signup.html'),
       },
     },
   },
   server: {
     fs: {
-      allow: ['..', 'src']
-    },
-    open: '/index.html',
-    configureServer({ app }) {
-      app.use((req, res, next) => {
-        if (req.url === '/') {
-          res.writeHead(302, { Location: '/index.html' });
-          res.end();
-        } else {
-          next();
-        }
-      });
+      allow: ['.']
     }
   }
 });
-
