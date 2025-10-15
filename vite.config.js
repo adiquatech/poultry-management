8c9200e0f7c016e204d41fc085b9f35466c18aa7
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-10-15 18:23:32 +0100

    another try

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
+  root: '.', // Project root is poultry-management/
   publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        growth: resolve(__dirname, 'growth/index.html'),
-        signin: resolve(__dirname, 'auth/signin.html'),
-        signup: resolve(__dirname, 'auth/signup.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        growth: resolve(__dirname, 'src/growth/index.html'),
+        signin: resolve(__dirname, 'src/auth/signin.html'),
+        signup: resolve(__dirname, 'src/auth/signup.html'),
       },
     },
   },
