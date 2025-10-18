ac1787a8e1e824badcb7f123c9258b6090b00683
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-10-18 15:34:03 +0100

    viteconfig issue

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,18 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.', 
+  root: 'src', 
   publicDir: 'public', 
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        growth: resolve(__dirname, 'src/growth/index.html'),
-        signin: resolve(__dirname, 'src/auth/signin.html'),
-        signup: resolve(__dirname, 'src/auth/signup.html'),
+        main: resolve(__dirname, 'index.html'),
+        growth: resolve(__dirname, '/growth/index.html'),
+        signin: resolve(__dirname, '/auth/signin.html'),
+        signup: resolve(__dirname, '/auth/signup.html'),
       },
     },
-  },
-  server: {
-    fs: {
-      allow: ['.', 'src'] 
-    },
-    open: '/index.html', 
   },
 });
